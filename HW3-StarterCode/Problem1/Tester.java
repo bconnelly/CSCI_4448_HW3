73ec712997742397ab95c466991d0540c7174b99
user <bryan.p.connelly@gmail.com>
2015-02-28 11:37:15 -0700

    Cleaned up Tester

--- HW3-StarterCode/Problem1/Tester.java
+++ HW3-StarterCode/Problem1/Tester.java
@@ -1,53 +1,23 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Tester{
     public static void main(String[] args){
-//        System.out.println("Testing graderQueue \nAdding Submissions to queue"
-//                + "\nType \"add\" or \"process\" to add or process submissions."
-//                + "\nType \"exit\" to exit.");
-//        
-//        Grader grader = new Grader();
-//        
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        
-//        String input = null;
-//        while(true){
-//            try{
-//                input = br.readLine();
-//            } catch(IOException e){
-//                System.out.println("IOException" + e);
-//                System.exit(1);
-//            }
-//            
-//            if(input.compareTo("add") == 0){
-//                Submission s = new Submission();
-//                grader.add(s);
-//            } else if(input.compareTo("process") == 0){
-//                grader.process();
-//            } else if(input .compareTo("exit") == 0){
-//                System.exit(1);
-//            } else{
-//                System.out.println("Please only type \"add\" or \"remove\"");
-//            }
-//        }
-        
+        //create one Grader object containing the queue that each
+    	//QueueProcessor will modify
     	Grader grader = new Grader();
-    	
-    	//create new QueueProcessor objects that can each modify the queue in grader
+    	//create new QueueProcessor objects
     	QueueProcessor a = grader.processorFactory();
     	QueueProcessor b = grader.processorFactory();
     	QueueProcessor c = grader.processorFactory();
     	QueueProcessor d = grader.processorFactory();
     	QueueProcessor e = grader.processorFactory();
     	QueueProcessor f = grader.processorFactory();
-    	
-    	//
+    	//create three submission objects to add to the queue
     	Submission s1 = new Submission();
     	Submission s2 = new Submission();
     	Submission s3 = new Submission();
-    	
+    	//add and remove from each of the QueueProcessor
     	a.add(s1);
     	b.add(s2);
     	c.add(s3);
@@ -57,3 +27,36 @@ public class Tester{
     	a.process();
     }
 }
+
+
+
+
+
+//System.out.println("Testing graderQueue \nAdding Submissions to queue"
+//+ "\nType \"add\" or \"process\" to add or process submissions."
+//+ "\nType \"exit\" to exit.");
+//
+//Grader grader = new Grader();
+//
+//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//String input = null;
+//while(true){
+//try{
+//input = br.readLine();
+//} catch(IOException e){
+//System.out.println("IOException" + e);
+//System.exit(1);
+//}
+//
+//if(input.compareTo("add") == 0){
+//Submission s = new Submission();
+//grader.add(s);
+//} else if(input.compareTo("process") == 0){
+//grader.process();
+//} else if(input .compareTo("exit") == 0){
+//System.exit(1);
+//} else{
+//System.out.println("Please only type \"add\" or \"remove\"");
+//}
+//}
